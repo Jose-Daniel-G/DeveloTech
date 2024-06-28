324237469bb2502e10d662fe4c7ee3898f96bcad
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2024-06-28 14:07:52 -0500

    advancing in login logout register

--- Assets/Scripts/UnityLoginLogoutRegister.cs
+++ Assets/Scripts/UnityLoginLogoutRegister.cs
@@ -1,64 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Threading;
 
 public class UnityLoginLogutRegister : MonoBehaviour
 {
-    [Header("Crear Cuenta")][Space]
+    [Space]
     public string baseUrl = "http://localhost/www/UnityLoginLogoutRegister/";
+
+    [Header("Crear Cuenta")]
+    [Space]
     public InputField username;
     public InputField password;
+    public InputField confirmPassword;
     public Text info;
 
-    public void AccountRegister(){
-        string user = username.text; 
-        string pass = password.text; 
-        StartCoroutine(RegisterNewAccount(user, pass)); 
+    [Header("Login")]
+    [Space]
+    public InputField usernameLog;
+    public InputField passwordLog;
+    public Text infoLog;
+
+    public void AccountRegister()
+    {
+        string user = username.text;
+        string pass = password.text;
+        string confirmPass = confirmPassword.text;
+        StartCoroutine(RegisterNewAccount(user, pass));
     }
 
-    public void AccountLogin(){
-        string user = username.text; 
-        string pass = password.text; 
-        StartCoroutine(LoginAccount(user, pass)); 
+    public void AccountLogin()
+    {
+        string user = usernameLog.text;
+        string pass = passwordLog.text;
+        StartCoroutine(LoginAccount(user, pass));
     }
 
-    IEnumerator RegisterNewAccount(string username, string password){
+    IEnumerator RegisterNewAccount(string username, string password)
+    {
         WWWForm form = new WWWForm();
         form.AddField("username", username);
         form.AddField("password", password);
-        using(UnityWebRequest www = UnityWebRequest.Post(baseUrl, form)){
+        using (UnityWebRequest www = UnityWebRequest.Post(baseUrl, form))
+        {
             www.downloadHandler = new DownloadHandlerBuffer();
             yield return www.SendWebRequest();
 
             if (www.isNetworkError)
             {
                 Debug.Log(www.error);
-            }else{
+            }
+            else
+            {
                 string responseText = www.downloadHandler.text;
-                Debug.Log("Response = "+responseText);
-                info.text = "Response = " +responseText;
+                Debug.Log("Response = " + responseText);
+                info.text = "Response = " + responseText;
             }
         }
     }
-    
-    IEnumerator LoginAccount(string username, string password){
+
+    IEnumerator LoginAccount(string username, string password)
+    {
         WWWForm form = new WWWForm();
-        form.AddField("username", username);
-        form.AddField("password", password);
-        using(UnityWebRequest www = UnityWebRequest.Post(baseUrl, form)){
+        form.AddField("loginUsername", username);
+        form.AddField("loginPassword", password);
+        using (UnityWebRequest www = UnityWebRequest.Post(baseUrl, form))
+        {
             www.downloadHandler = new DownloadHandlerBuffer();
             yield return www.SendWebRequest();
 
             if (www.isNetworkError)
             {
                 Debug.Log(www.error);
-            }else{
+            }
+            else
+            {
                 string responseText = www.downloadHandler.text;
-                Debug.Log("Response = "+responseText);
-                info.text = "Response = " +responseText;
+                Debug.Log("Response = " + responseText);
+                info.text = "Response = " + responseText;
+                
             }
         }
     }
--- UnityLoginLogoutRegister/index.php
+++ UnityLoginLogoutRegister/index.php
@@ -7,22 +7,33 @@ if (isset($_POST["username"])){
     $password = mysqli_real_escape_string($connection, $_POST["password"]);
     //check they are empty?
     if ($username != "" && $password != "") {
-        mysqli_query($connection, "INSERT INTO users(username, password) VALUES('$username', '$password')");
-        echo "Registrando nueva cuenta: Usuario ".$username." y contraseña: ".$password;
+        //Check if the username has not taken
+        if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM users WHERE username = '$username'")) == 0) {
+            mysqli_query($connection, "INSERT INTO users(username, password) VALUES('$username', '$password')");
+            echo "Registrando nueva cuenta: Usuario ".$username." y contraseña: ".$password;
+        }else{
+            echo "Este usuario no esta disponible. Por favor cree otro usuario";
+        }
     }else{
         echo "Ambos campos son requeridos";
     }
 
-// }else if (isset($_POST["LoginUsername"])){
-//     $username = mysqli_real_escape_string($connection, $_POST["loginUsername"]);
-//     $username = mysqli_real_escape_string($connection, $_POST["loginPassword"]);
-//     //check they are empty?
-//     if ($username != "" && $password != "") {
-//         mysqli_query($connection, "INSERT INTO users(username, password) VALUES('$username', '$password')");
-//         echo "Iniciando sesión: Usuario ".$username." y contraseña: ".$password;
-//     }else{
-//         echo "Ambos campos son requeridos";
-//     }
+}else if (isset($_POST["loginUsername"])){
+    $username = mysqli_real_escape_string($connection, $_POST["loginUsername"]);
+    $password = mysqli_real_escape_string($connection, $_POST["loginPassword"]);
+    //check they are empty?
+    if ($username != "" && $password != "") {
+        //Check are entered username and password matched
+        $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+        if (mysqli_num_rows(mysqli_query($connection, $sql)) > 0) {
+            echo "Inicio exitoso! Usuario ".$username." y Contraseña ".$password;
+            
+        }else{
+            echo "Fallo de Inicio! Usuario incorrecto o/y Contraseña";
+        }
+    }else{
+        echo "Ambos campos son requeridos";
+    }
 }else{
     echo "Unity Login, Logout and Register";
 }
